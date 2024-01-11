7217d0398685d04c5d0b3ce5c81d56e2f6076629
ThaiMing3 <tanthaiming@gmail.com>
2024-01-11 14:52:43 +0800

    Fix lint errors

--- frontend/components/Form/Password.vue
+++ frontend/components/Form/Password.vue
@@ -5,8 +5,8 @@
       type="button"
       class="inline-flex p-1 ml-1 justify-center mt-auto mb-3 tooltip absolute top-11 right-3"
       data-tip="Toggle Password Show"
-      @click="toggle()"
       aria-label="Toggle Password Show"
+      @click="toggle()"
     >
       <Icon name="mdi-eye" class="h-5 w-5" />
     </button>
